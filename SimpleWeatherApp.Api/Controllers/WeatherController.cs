ea79147bb93713207407255baae27de470bba2d8
Deepesh Kapadia <dkapadia@deloitte.com.au>
2022-07-18 15:51:59 +1000

    Updates to WeatherController get method logic

--- SimpleWeatherApp.Api/Controllers/WeatherController.cs
+++ SimpleWeatherApp.Api/Controllers/WeatherController.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Logging;
 using SimpleWeatherApp.Api.Models;
 using SimpleWeatherApp.Api.Attributes;
 using Microsoft.Extensions.Options;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace SimpleWeatherApp.Api.Controllers
 {
@@ -25,19 +27,46 @@ namespace SimpleWeatherApp.Api.Controllers
 
         [HttpGet]
         [RequestLimit(MaxRequests = 5, TimeLimit = 60)]
-        public string Get(string city, string country)
+        public async Task<ActionResult> Get(string city, string country)
         {
-            if(!string.IsNullOrEmpty(city) && !string.IsNullOrEmpty(country))
+            if (!string.IsNullOrEmpty(city) && !string.IsNullOrEmpty(country))
             {
                 string appId = _weatherSettings.APIKeyList[0];
 
                 string url = $"{_weatherSettings.BaseAddress}?q={city},{country}&appid={appId}";
-                
-                //TODO - make a call to above URL and retrieve data
 
-                return string.Empty;
+                using (var client = new HttpClient())
+                {
+                    try
+                    {
+                        client.BaseAddress = new Uri(_weatherSettings.BaseAddress);
+                        var response = await client.GetAsync($"?q={city},{country}&appid={appId}");
+                        response.EnsureSuccessStatusCode();
+
+                        var stringResult = await response.Content.ReadAsStringAsync();
+                        var weatherResponse = JsonConvert.DeserializeObject<OpenWeatherReponse>(stringResult);
+
+                        if (weatherResponse != null && weatherResponse.weather.Count > 0)
+                        {
+                            return Ok(new
+                            {
+                                Message = string.Join(",", weatherResponse.weather.Select(x => x.description))
+                            });
+                        }
+
+                        return Ok(new
+                        {
+                            Message = "No weather description returned"
+                        });
+                    }
+                    catch (HttpRequestException httpRequestException)
+                    {
+                        return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                    }
+                }
             }
-            return "hello";
+
+            return BadRequest("Missing parameters - city and country");
         }
     }
 }
